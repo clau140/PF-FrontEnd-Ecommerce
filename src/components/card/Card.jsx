b96fdee39fb945a0883fd22e27f0e726a0724a8b
Alisson <alissonmartin2410@gmail.com>
2024-05-20 20:25:31 -0500

    Update Fron Searchbar-Navbar-Cards-Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,58 @@
-const Card = () => {
-
-    return (
-        <div></div>
-    )
+const Card = ({ template }) => {
+  return (
+    <div
+      className="max-w-xs rounded relative overflow-hidden shadow-lg transition-transform transform hover:scale-105"
+      key={template.id}
+    >
+        <div className="flex items-center ml-4 mt-4 mb-2">
+            {[...Array(5)].map((_, index) => (
+              <svg
+                key={index}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="none"
+                stroke="#f7c948"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10 1l2.705 5.674L18 7.5l-4.073 4.096L15.5 18 10 14.8 4.5 18l1.573-6.404L2 7.5l5.295-.826z"
+                />
+              </svg>
+            ))}
+          </div>
+      <img className="w-full" src={template.image} alt={template.name} />
+      <div className="absolute top-0 right-0 m-2 z-10 opacity-1 transition-opacity duration-300">
+        
+          <svg
+            className="h-4 w-4 text-slate-800 mt-1"
+            fill="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor" 
+          >
+            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+          </svg>
+        
+      </div>
+      <div className="px-6 py-4">
+        <div className="font-inter text-l">{template.name}</div>
+        <div className="flex justify-between items-center">
+          <p className="text-gray-700 text-lg font-bold ml-auto">${template.price}</p>
+        </div>
+      </div>
+      <div className="px-6 py-4">
+        <button className="w-full bg-slate-700 hover:bg-slate-800 text-white font-bold py-2 px-4 rounded">
+          Add To Cart 
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,8 +1,20 @@
-const Cards = () => {
+import Card from "../card/Card";
 
-    return (
-        <div></div>
-    )
+const Cards = ({templates}) => {
+    const templatesCard = templates;
+  return (
+    <div className="mx-auto w-70 ">
+    <div className="flex justify-end">
+        <div className="grid grid-cols-3 gap-8 mt-12 mr-12 mb-12">
+                {templatesCard?.map((template) => (
+                    <Card template={template} key={template.id} 
+                    />
+                ))}
+        </div>
+    </div>
+</div>
+
+  );
 };
 
 export default Cards;
