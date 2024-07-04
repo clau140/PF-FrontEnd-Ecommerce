79609b289e50c36d58cd64990995921622ec299c
ValcarC <CorreoUsuario@correo.com>
2024-07-03 21:43:31 -0600

    update

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,36 +1,34 @@
-import React from "react";
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
-import ImageGallery from 'react-image-gallery'
+import ImageGallery from 'react-image-gallery';
 import { Rating } from "@mui/material";
 import { getTemplateById, getCategories } from "../../redux/actions/templatesAction";
-import "react-image-gallery/styles/css/image-gallery.css"
+import "react-image-gallery/styles/css/image-gallery.css";
 import 'react-toastify/dist/ReactToastify.css';
 import ReviewForm from '../../components/reviews/ReviewForm';
 import { getReviewsTemplate } from "../../redux/actions/reviewsAction";
 import { promedio } from "./promedio";
 import { addToCart } from "../../redux/actions/cartActions";
 import { ToastContainer } from "react-toastify";
-
+import { useTranslation } from 'react-i18next';
 
 const Detail = () => {
+    const { t, i18n } = useTranslation();
+    const changeLanguage = (lng) => i18n.changeLanguage(lng);
 
     const { id } = useParams();
     const dispatch = useDispatch();
-    const [ isModalOpen, setIsModalOpen ] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [images, setImages] = useState([]);
+    const [hasUserReviewed, setHasUserReviewed] = useState(false); 
 
-    const [ images, setImages ] = useState([]);
-    let template = useSelector((state) => state.templates.detailTemplate);
-    const reviews = useSelector((state) => state.templates.reviews);
 
-    useEffect(() => {
-        dispatch(getTemplateById(id))
-            .then(() => {
-                dispatch(getReviewsTemplate(id));
-            });
-    }, [ id, dispatch ]);
+    
+    const template = useSelector((state) => state.templates.detailTemplate);
+    const allReviews = useSelector((state) => state.templates.detailTemplateCopy.reviews) || [];
+    const loggedIn = useSelector((state) => state.user.loggedIn); 
+    const userId = useSelector(state => state.user.userInfo.id); 
 
     useEffect(() => {
         if (template && template.images) {
@@ -40,7 +38,7 @@ const Detail = () => {
 
     useEffect(() => {
         dispatch(getTemplateById(id)).then((response) => {
-            console.log('getTemplateById:', response); 
+            console.log('Response de getTemplateById:', response); 
             if (response && response.payload && response.payload.reviews) {
                 const userReview = response.payload.reviews.find(review => review.idUser === userId);
                 console.log('User Review encontrado:', userReview);
@@ -48,37 +46,23 @@ const Detail = () => {
             }
         });
         dispatch(getCategories());
-        dispatch(getReviewsTemplate(id)).then((response) => {
-            console.log("getReviewsTemplate:", response);
-        });
+        dispatch(getReviewsTemplate(id))
     }, [id, dispatch, userId]);
 
+    
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
     const categories = template.categories || [];
     const technologies = template.technologies || [];
-
-    const allReviews = useSelector((state) => state.templates.detailTemplateCopy.reviews) || [];
     const ratings = allReviews.map((e) => e.rating) || [];
-    let resultRating = ratings.length > 0 ? promedio(ratings) : 0;
-
-    useEffect(() => {
-        dispatch(getTemplateById(id));
-        dispatch(getCategories());
-        dispatch(getReviewsTemplate(id));
-    }, [ id, dispatch ]);
-
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+    const resultRating = ratings.length > 0 ? promedio(ratings) : 0;
 
     return (
         <div>
-          <ToastContainer />
-          <div className="p-4 shadow-md">
+            <ToastContainer />
+            <div className="p-4 shadow-md">
     <div className="bg-gray relative object-cover mx-auto min-w-[20rem] w-full rounded-2xl flex flex-col md:flex-row  mb-10 shadow-md border-2">
       <div className="bg-white w-[70%] object-cover mb-5 mt-10 mr-10 relative overflow-hidden flex items-center justify-center ml-10 ">
         <ImageGallery
@@ -102,7 +86,7 @@ const Detail = () => {
           <br />
           <div className="flex items-center ">
             <div className="flex flex-row gap-4">
-              <Rating className="ml-2 text-sm" readOnly value={template?.rating} />
+              <Rating className="ml-2 text-sm" readOnly value={resultRating ? resultRating : 0}/>
             </div>
           </div>
 
@@ -125,6 +109,9 @@ const Detail = () => {
                     ))}
                 </div>
             </div>
+
+
+
 
 
           <h3 className="ml-3 text-start text-sm text-bggris  mr-8 mt-4 font-normal text-gray-800 pb-4 border-green-900">
@@ -160,53 +147,65 @@ const Detail = () => {
                     </div>
                 </div>
 
-                { allReviews.length > 0 ? (
+                <div className="relative mx-auto min-w-[20rem] w-full rounded-2xl flex flex-col md:flex-row items-center md:justify-start mb-10">
+    {loggedIn ? (
+        hasUserReviewed ? (
+            <p className="text-red-600">Ya has realizado una opinión sobre esta plantilla.</p>
+        ) : (
+            <button
+                onClick={openModal}
+                className="bg-[#06B6D4] text-white font-inter hover:scale-110 hover:bg-green-500 font-bold py-2 px-4 rounded-full"
+            >
+                Opinar
+            </button>
+        )
+    ) : (
+        <div className="inline-flex items-center space-x-1">
+            <Link to="/SignIn" className="text-blue-500 hover:underline">Inicia sesión</Link>
+            <p className="text-gray-800">para dejar una opinión</p>
+        </div>
+    )}
+</div>
+
+                {allReviews.length > 0 ? (
                     <div className="bg-gray relative mx-auto min-w-[20rem] w-full rounded-2xl flex flex-col md:flex-row mb-10 shadow-md border-2">
                         <div className="bg-white mr-10 relative overflow-hidden ml-10">
-                            <h2 className="text-start text-xl mr-8 mt-4 font-inter font-bold text-gray-800 pb-4 transition-colors tracking-wider border-green-900">
-                                Reviews
+                            <h2 className="text-start text-xl mr-8 mt-4 font-inter font-bold text-gray-800 pb-4 transition-colors tracking-wider border-green-500">
+                                Reseñas
                             </h2>
-                            { allReviews.map((r) => (
-                                <div key={ r.id } className="mb-4">
-                                    <Rating className="mb-2" readOnly value={ r.rating } />
-                                    <p className="text-gray-600">{ r.date }</p>
-                                    <span className="text-gray-800">{ r.content }</span>
+                            {allReviews.map((r) => (
+                                <div key={r.id} className="mb-4">
+                                    <Rating className="mb-2" readOnly value={r.rating} />
+                                    <p className="text-gray-600">{r.date}</p>
+                                    <span className="text-gray-800">{r.content}</span>
                                 </div>
-                            )) }
+                            ))}
                         </div>
                     </div>
                 ) : (
                     <div className="bg-gray-100 relative mx-auto min-w-[20rem] w-full rounded-2xl flex flex-col md:flex-row mb-10 shadow-md border-2">
-                        <div className="bg-zinc-50 text-lg font-inter font-semibold p-3">
+                        <div className="bg-zinc-50 text-lg font p-3">
                             <span>No existen opiniones de este producto</span>
                         </div>
                     </div>
-                ) }
-                <div className=" relative mx-auto min-w-[20rem] w-full rounded-2xl flex flex-col md:flex-row mb-10 ">
-                    <button
-                        onClick={ openModal }
-                        className="bg-blue-500 text-white font-inter hover:bg-blue-700 font-bold py-2 px-4 rounded-full"
-                    >
-                        Opinar
-                    </button>
-                </div>
+                )}
 
-                { isModalOpen && (
-                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                        <div className="relative bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+                {isModalOpen && (
+                    <div className="fixed inset-0 flex items-start justify-center bg-black bg-opacity-50 z-50">
+                        <div className="relative bg-white p-6 rounded-lg shadow-lg w-full max-w-md mt-8">
                             <button
-                                onClick={ closeModal }
+                                onClick={closeModal}
                                 className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 focus:outline-none"
                             >
                                 X
                             </button>
-                            <ReviewForm templateId={ id } />
+                            <ReviewForm templateId={id} />
                         </div>
                     </div>
-                ) }
+                )}
             </div>
         </div>
     );
-}
+};
 
 export default Detail;
